b5d70579af7293515186c88900ff77256162dc32
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2025-03-10 18:44:06 +0900

    NonSlipMat

--- data_collection.py
+++ data_collection.py
@@ -79,7 +79,7 @@ def main(argv):
     vis3d = gs3drecon.Visualize3D(dev.imgh, dev.imgw, '', mmpp)
     
     # texture_path = input('Input the texture label: ')
-    texture_path = 'Jeans'
+    texture_path = 'NonSlipMat'
     texture_path = 'Texture/' + texture_path
     if not os.path.exists(texture_path):
         os.makedirs(texture_path)
