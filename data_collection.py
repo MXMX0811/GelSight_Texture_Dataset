6e335373968b7fb1bb871ffce2ad4b5414021290
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2025-04-01 19:02:57 +0900

    commit

--- data_collection.py
+++ data_collection.py
@@ -94,7 +94,7 @@ def main(argv):
     vis3d = gs3drecon.Visualize3D(dev.imgh, dev.imgw, '', mmpp)
     
     # texture_path = input('Input the texture label: ')
-    texture_path = 'CopperMesh1'
+    texture_path = 'CopperMesh2'
     texture_path = 'Texture/' + texture_path
     if not os.path.exists(texture_path):
         os.makedirs(texture_path)
