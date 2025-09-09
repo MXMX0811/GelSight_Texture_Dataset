5d2782398298b365c9aa728f58097f4f2af7a586
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2025-09-10 01:50:02 +0900

    Generate prompts and save json files

---
+++ MiniCPM_prompt_generator.py
@@ -0,0 +1,82 @@
+'''
+Author: Mingxin Zhang m.zhang@hapis.k.u-tokyo.ac.jp
+Date: 2025-08-26 03:33:21
+LastEditors: Mingxin Zhang
+LastEditTime: 2025-09-10 01:43:41
+Copyright (c) 2025 by Mingxin Zhang, All Rights Reserved. 
+'''
+import os
+import torch
+import json
+import random
+from PIL import Image
+from transformers import AutoModel, AutoTokenizer, BitsAndBytesConfig
+from pathlib import Path
+from download import download_model
+
+# Script path
+DIR = str(Path(__file__).resolve().parent)
+
+def get_texture_folders(root_dir):
+    return [os.path.join(root_dir, texture) for texture in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, texture))]
+
+if __name__ == "__main__":
+    # Download the model
+    model_path = DIR + '/models/'
+    model_name = 'MiniCPMv2_6-prompt-generator'
+    download_model(
+        repo_id='pzc163/MiniCPMv2_6-prompt-generator',
+        dest_dir=model_path + model_name,
+        revision='main',
+        token=os.getenv('HUGGING_FACE_HUB_TOKEN')
+    )
+
+    # Load
+    model = AutoModel.from_pretrained(model_path + model_name, trust_remote_code=True, torch_dtype=torch.bfloat16, local_files_only=True).eval()
+    model = model.to(device='cuda')
+    
+    print(f"Model Parameters: {sum(p.numel() for p in model.parameters()):,}")
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path + model_name, trust_remote_code=True)
+
+    texture_folders = get_texture_folders(DIR + '/Texture/')
+    for image_path in texture_folders:
+        texture_name = image_path.split("/")[-1]
+        # write json for prompts of each texture
+        if os.path.exists(image_path + '/prompts.json'):
+            with open(image_path + '/prompts.json', 'r') as f:
+                prompts = json.load(f)
+                
+        else:
+            prompts = {}
+            prompts[str(texture_name)] = {}
+            
+        prompts[str(texture_name)][str(model_name)] = {}
+
+        sampled_i = random.sample(range(1, 21), 5)
+        for i in range(5): # generate 5 prompts for each texture
+            image = Image.open(image_path + '/' + str(sampled_i[i]) + '.jpg').convert('RGB')
+            question = 'Carefully observe the surface texture shown in the image and describe its tactile characteristics in detail. \
+                            Your description should include:\
+                            •	Roughness level (e.g., smooth, fine, grainy, prickly)\
+                            •	Macroscopic structure (e.g., patterned ridges, grooves, undulations)\
+                            •	Microscopic structure (e.g., tiny granules, fuzziness, porous feel)\
+                            •	Inferred tactile impression when pressing vertically on the shown surface or stroking the surface horizontally according to the material (such as soft, rigid)\
+                            Generate an English passage that conveys the tactile features of this material, suitable for use in a haptic feedback generation task.'
+            msgs = [{'role': 'user', 'content': question}]
+
+            res = model.chat(
+                image=image,
+                msgs=msgs,
+                tokenizer=tokenizer,
+                sampling=True, # if sampling=False, beam_search will be used by default
+                temperature=0.7,
+                # system_prompt='' # pass system_prompt if needed
+            )
+            
+            prompts[str(texture_name)][str(model_name)][str(i+1)] = res
+        # save json
+        with open(image_path + '/prompts.json', 'w') as f:
+            json.dump(prompts, f, indent=4)
+        print(f"Prompts for {texture_name} saved.")
+        break
