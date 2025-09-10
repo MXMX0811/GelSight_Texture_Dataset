f6a2a4ff2c3846a6e797dc6b3767bae22fa76729
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2025-09-11 01:36:45 +0900

    Added DeepSeek JanusPro7B

--- JanusPro7B_generator.py
+++ JanusPro7B_generator.py
@@ -2,7 +2,7 @@
 Author: Mingxin Zhang m.zhang@hapis.k.u-tokyo.ac.jp
 Date: 2025-08-26 03:33:21
 LastEditors: Mingxin Zhang
-LastEditTime: 2025-09-11 01:01:34
+LastEditTime: 2025-09-11 01:36:11
 Copyright (c) 2025 by Mingxin Zhang, All Rights Reserved. 
 '''
 import os
@@ -10,10 +10,13 @@ import torch
 import json
 import random
 from PIL import Image
-from transformers import AutoModel, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from janus.models import MultiModalityCausalLM, VLChatProcessor
+from janus.utils.io import load_pil_images
 from pathlib import Path
 from download import download_model
 
+# Run pip install -e . in the dir models/Janus to install the Janus package first
 # Script path
 DIR = str(Path(__file__).resolve().parent)
 
@@ -23,21 +26,22 @@ def get_texture_folders(root_dir):
 if __name__ == "__main__":
     # Download the model
     model_path = DIR + '/models/'
-    model_name = 'MiniCPMv2_6-prompt-generator'
+    model_name = 'Janus-Pro-7B'
     download_model(
-        repo_id='pzc163/MiniCPMv2_6-prompt-generator',
+        repo_id='deepseek-ai/Janus-Pro-7B',
         dest_dir=model_path + model_name,
         revision='main',
         token=os.getenv('HUGGING_FACE_HUB_TOKEN')
     )
 
     # Load
-    model = AutoModel.from_pretrained(model_path + model_name, trust_remote_code=True, torch_dtype=torch.bfloat16, local_files_only=True).eval()
-    model = model.to(device='cuda')
+    vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path + model_name)
+    tokenizer = vl_chat_processor.tokenizer
     
-    print(f"Model Parameters: {sum(p.numel() for p in model.parameters()):,}")
-
-    tokenizer = AutoTokenizer.from_pretrained(model_path + model_name, trust_remote_code=True)
+    vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(model_path + model_name, trust_remote_code=True)
+    vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+    
+    print(f"Model Parameters: {sum(p.numel() for p in vl_gpt.parameters()):,}")
 
     texture_folders = get_texture_folders(DIR + '/Texture/')
     for image_path in texture_folders:
@@ -55,7 +59,7 @@ if __name__ == "__main__":
 
         sampled_i = random.sample(range(1, 21), 5)
         for i in range(5): # generate 5 prompts for each texture
-            image = Image.open(image_path + '/' + str(sampled_i[i]) + '.jpg').convert('RGB')
+            image = image_path + '/' + str(sampled_i[i]) + '.jpg'
             question = 'Carefully observe the surface texture shown in the image and describe its tactile characteristics.\
                         You should include detailed quantitative descriptions of the following aspects:\
                         •	Roughness level (e.g., smooth, fine, grainy, prickly)\
@@ -68,18 +72,40 @@ if __name__ == "__main__":
                         Do not use subjects and introductory phrases at the beginning of the response.\
                         No filler.\
                         Use English to answer.'
-            msgs = [{'role': 'user', 'content': question}]
+                        
+            conversation = [
+                {
+                    "role": "<|User|>",
+                    "content": f"<image_placeholder>\n{question}",
+                    "images": [image],
+                },
+                {"role": "<|Assistant|>", "content": ""},
+            ]
+            
+            # load images and prepare for inputs
+            pil_images = load_pil_images(conversation)
+            prepare_inputs = vl_chat_processor(
+                conversations=conversation, images=pil_images, force_batchify=True
+            ).to(vl_gpt.device)
 
-            res = model.chat(
-                image=image,
-                msgs=msgs,
-                tokenizer=tokenizer,
-                sampling=True, # if sampling=False, beam_search will be used by default
-                temperature=0.7
-                # system_prompt='' # pass system_prompt if needed
+            # # run image encoder to get the image embeddings
+            inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+
+            # # run the model to get the response
+            outputs = vl_gpt.language_model.generate(
+                inputs_embeds=inputs_embeds,
+                attention_mask=prepare_inputs.attention_mask,
+                pad_token_id=tokenizer.eos_token_id,
+                bos_token_id=tokenizer.bos_token_id,
+                eos_token_id=tokenizer.eos_token_id,
+                max_new_tokens=512,
+                do_sample=False,
+                use_cache=True,
             )
             
-            prompts[str(texture_name)][str(model_name)][str(i+1)] = res
+            answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+            prompts[str(texture_name)][str(model_name)][str(i+1)] = answer
+            
         # save json
         with open(image_path + '/prompts.json', 'w') as f:
             json.dump(prompts, f, indent=4)
