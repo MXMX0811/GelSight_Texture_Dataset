70e36ef563f556d70e5c8e97440aa951407fa7d8
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2025-11-05 19:01:20 +0900

    Scarf

--- data_collection.py
+++ data_collection.py
@@ -105,7 +105,7 @@ def main(argv):
     vis3d = gs3drecon.Visualize3D(dev.imgh, dev.imgw, '', mmpp)
     
     # texture_path = input('Input the texture label: ')
-    texture_path = 'FloorMop' 
+    texture_path = 'Scarf' 
     texture_path = 'Texture/' + texture_path
     if not os.path.exists(texture_path):
         os.makedirs(texture_path)
